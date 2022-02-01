010a57cb3335cf4c37c89d93593190f50cc3e397
Twinq <mikhlikdv@gmail.com>
2022-02-01 22:10:20 +0300

    module.1_2 cat-age

---
+++ js/module.1_2.js
@@ -0,0 +1,13 @@
+const myAge = 23;
+
+let earlyYears = 2;
+earlyYears = earlyYears * 12;
+
+let laterYears = myAge - 2;
+laterYears = laterYears * 4;
+
+let myAgeInCatYears = earlyYears + laterYears;
+
+const myName = 'Даниил'.toUpperCase();
+
+console.log(`Меня зовут ${myName} мне ${myAge} года, а в кошачих годах мне ${myAgeInCatYears} лет.`);
