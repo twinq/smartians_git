126d2b94bc916afbd5c7a8e98b2a1f8c0b910b66
Twinq <mikhlikdv@gmail.com>
2022-02-08 23:53:24 +0300

    dont understand

---
+++ js/module.3game.js
@@ -0,0 +1,44 @@
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'камень' || userInput === 'ножницы' || userInput === 'бумага') {
+        return userInput;
+    } else {
+        console.warn('Ошибка!');
+    }
+}
+
+const getComputerChoice = () => {
+    let computerInput = Math.floor(Math.random() * 3);
+
+    switch (computerInput) {
+        case 0:
+            return 'камень';
+        case 1:
+            return 'ножницы';
+        case 2:
+            return 'бумага';
+    }
+}
+
+
+const determineWinner = (userChoice, computerChoice) => {
+    if ((userChoice === 'камень' && computerChoice === 'ножницы') 
+    || (userChoice === 'ножницы' && computerChoice === 'бумага')
+    || (userChoice === 'бумага' && computerChoice === 'камень')) {
+        return 'победа';
+    } else if ((userChoice === 'камень' && computerChoice === 'бумага') 
+    || (userChoice === 'ножницы' && computerChoice === 'камень')
+    || (userChoice === 'бумага' && computerChoice === 'ножницы')) {
+        return 'поражение';
+    } else {
+        return 'ничья';
+    }
+}
+
+const playGame = () => {
+    let userChoice = getUserChoice('Камень');
+    let computerChoice = getComputerChoice();
+    determineWinner(userChoice, computerChoice);
+    console.log(determineWinner(userChoice, computerChoice))
+}
+playGame();
