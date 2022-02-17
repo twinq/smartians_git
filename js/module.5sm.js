2990aa8f4c6a868fdec6f1ea24680f04ba470e42
Twinq <mikhlikdv@gmail.com>
2022-02-17 23:07:27 +0300

    create secret massage

---
+++ js/module.5sm.js
@@ -0,0 +1,39 @@
+let secretMessage = [
+    'вчера',
+    ',',
+    'что',
+    'к',
+    'нам',
+    'в',
+    'Новом',
+    'году',
+    'крупнейшим',
+    'интернет-бизнесом',
+    'будет',
+    'бухгалтерская',
+    'компания',
+    ',',
+    'о',
+    'которой',
+    'еще',
+    'никто',
+    'ничего'
+   ];
+
+   console.log(secretMessage.length);
+
+   secretMessage.pop();
+   console.log(secretMessage.length);
+
+   secretMessage.push('не', 'знает');
+
+   console.log(secretMessage.indexOf('бухгалтерская'));
+   secretMessage[11] = 'образовательная';
+
+   secretMessage.shift();
+   secretMessage.unshift('Предполагаю');
+
+   console.log(secretMessage.indexOf('нам'));
+   secretMessage.splice(4, 6, '2030');
+   
+   console.log(secretMessage.join(' '));
